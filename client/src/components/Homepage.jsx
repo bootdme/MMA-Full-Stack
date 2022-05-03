34def8a37ff43c73792330eb23a07f4f1f239baf
David Estephanian <de@Davids-Air.attlocal.net>
2022-05-03 09:35:21 -0700

    added react router to project and continued styling

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { HomepageWrapper } from '../styles/Homepage.styled';
+import { HomepageOuter, HomepageWrapper } from '../styles/Homepage.styled';
 
 function Homepage() {
   return (
-    // <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <HomepageWrapper>
-        <header className="title">MMA Title</header>
-        <div className="options">
-          <span>Text 1</span>
-          <span>Text 2</span>
-        </div>
-      </HomepageWrapper>
-    // </div>
+    <HomepageOuter>
+      <div className="homepage">
+        <HomepageWrapper>
+          <header className="title">MMA Title</header>
+          <header className="options">
+            <span>Add A Fighter</span>
+            <span>One-on-One</span>
+          </header>
+        </HomepageWrapper>
+      </div>
+    </HomepageOuter>
   );
 }
 
--- client/src/styles/Homepage.styled.js
+++ client/src/styles/Homepage.styled.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+export const HomepageOuter = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export const HomepageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 1000px;
+  width: 750px;
   align-items: center;
   justify-content: center;
   border: 1px solid black;
