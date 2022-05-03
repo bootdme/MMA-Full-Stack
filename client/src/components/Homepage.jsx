316de3244c0d3721f06b5be8740a4cc6b12a0cee
David Estephanian <de@Davids-Air.attlocal.net>
2022-05-02 23:39:00 -0700

    thanks amy

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -3,9 +3,14 @@ import { HomepageWrapper } from '../styles/Homepage.styled';
 
 function Homepage() {
   return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
     <HomepageWrapper>
       <header className="Title">MMA Title</header>
+      <div style={{display: 'flex'}}>
+        <span>Text 1</span><span>Text 2</span>
+      </div>
     </HomepageWrapper>
+    </div>
   );
 }
 
--- client/src/styles/Homepage.styled.js
+++ client/src/styles/Homepage.styled.js
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 
 export const HomepageWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 1000px;
+  align-items: center;
+  justify-content: center;
   border: 1px solid black;
 `;
