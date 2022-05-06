e51b7605aa8ae237504629781079ceb07c2704be
David Estephanian <desteph@protonmail.com>
2022-05-06 10:56:23 -0700

    Need to fix styling

--- client/src/components/FighterList.jsx
+++ client/src/components/FighterList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function FighterList({ fighters }) {
   return (
-    <div className="fighter-card flex-col">
+    <div className="fighter-card flex">
       {fighters.map((fighter, index) => (
-        <div className="bg-gray-200 w-full flex justify-center items-center" key={index}>
+        <span className="bg-gray-200 w-full flex justify-center items-center flex-wrap" key={index}>
           <div className="w-60 p-2 bg-white rounded-xl">
             <img className="h-40 object-cover rounded-xl" src={`https://www.sherdog.com/${fighter.imageUrl}`} />
             <div className="p-2">
@@ -12,7 +12,7 @@ function FighterList({ fighters }) {
               <p className="text-sm text-gray-600">Age: {fighter.age || 'N/A'}</p>
             </div>
           </div>
-        </div>
+        </span>
       ))}
     </div>
   );
